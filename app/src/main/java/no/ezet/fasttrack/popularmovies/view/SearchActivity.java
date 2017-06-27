5b7387bce234f78f4c08de266cc92ca9ef584a78
Lars-Kristian Dahl <lars.dahl@gmail.com>
2017-06-27 10:56:09 +0200

    Add SearchActivity

---
+++ app/src/main/java/no/ezet/fasttrack/popularmovies/view/SearchActivity.java
@@ -0,0 +1,15 @@
+package no.ezet.fasttrack.popularmovies.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import no.ezet.fasttrack.popularmovies.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
