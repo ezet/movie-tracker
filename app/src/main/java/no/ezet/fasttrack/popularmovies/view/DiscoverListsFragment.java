24e9f56b9e472a3468c498a765038092dc1940f6
Lars-Kristian Dahl <lars.dahl@gmail.com>
2017-06-28 11:10:03 +0200

    Refactor fragments for bottom navigation

---
+++ app/src/main/java/no/ezet/fasttrack/popularmovies/view/DiscoverListsFragment.java
@@ -0,0 +1,120 @@
+package no.ezet.fasttrack.popularmovies.view;
+
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import no.ezet.fasttrack.popularmovies.R;
+import no.ezet.fasttrack.popularmovies.viewmodel.MovieListViewModel;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DiscoverListsFragment extends Fragment {
+
+    private TabLayout tabLayout;
+    private AppBarLayout appBarLayout;
+    private TabLayoutHost host;
+
+    public static DiscoverListsFragment create() {
+        return new DiscoverListsFragment();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        host = (TabLayoutHost) context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_discover_lists, container, false);
+        ViewPager viewPager = (ViewPager) root.findViewById(R.id.view_pager);
+        viewPager.setAdapter(new DiscoverPageAdapter(getResources(), getActivity().getSupportFragmentManager()));
+
+        appBarLayout = host.getAppBarLayout();
+        tabLayout = (TabLayout) inflater.inflate(R.layout.tablayout_discover_lists, appBarLayout, false);
+        tabLayout.setupWithViewPager(viewPager);
+
+        appBarLayout.addView(tabLayout);
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        appBarLayout.removeView(tabLayout);
+    }
+
+    public interface TabLayoutHost {
+
+        AppBarLayout getAppBarLayout();
+
+    }
+
+    private static class DiscoverPageAdapter extends FragmentPagerAdapter {
+
+        private final Resources resources;
+
+        DiscoverPageAdapter(Resources resources, FragmentManager fm) {
+            super(fm);
+            this.resources = resources;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            int resId;
+            switch (position) {
+                case MovieListViewModel.POPULAR:
+                    resId = R.string.title_popular;
+                    break;
+                case MovieListViewModel.UPCOMING:
+                    resId = R.string.title_upcoming;
+                    break;
+                case MovieListViewModel.TOP_RATED:
+                    resId = R.string.title_top_rated;
+                    break;
+                case MovieListViewModel.NOW_PLAYING:
+                    resId = R.string.title_now_playing;
+                    break;
+                default:
+                    throw new IllegalArgumentException();
+            }
+            return resources.getString(resId);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            switch (i) {
+                case MovieListViewModel.POPULAR:
+                    return MovieListFragment.create(MovieListViewModel.POPULAR);
+                case MovieListViewModel.UPCOMING:
+                    return MovieListFragment.create(MovieListViewModel.UPCOMING);
+                case MovieListViewModel.TOP_RATED:
+                    return MovieListFragment.create(MovieListViewModel.TOP_RATED);
+                case MovieListViewModel.NOW_PLAYING:
+                    return MovieListFragment.create(MovieListViewModel.NOW_PLAYING);
+                default:
+                    throw new IllegalArgumentException();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+    }
+
+}
--- app/src/main/java/no/ezet/fasttrack/popularmovies/view/MovieListBaseFragment.java
+++ app/src/main/java/no/ezet/fasttrack/popularmovies/view/MovieListBaseFragment.java
@@ -7,19 +7,14 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.inject.Inject;
 
@@ -151,96 +146,6 @@ public abstract class MovieListBaseFragment<T extends MovieListBaseViewModel> ex
         void onItemClick(View f, MovieListItem movie);
     }
 
-    private static class MovieListRecyclerViewAdapter
-            extends RecyclerView.Adapter<MovieListRecyclerViewAdapter.ViewHolder> {
-
-        private final List<MovieListItem> movies = new ArrayList<>();
-        private ImageService imageService;
-        private FragmentListener listener;
-        private int counter;
-
-        MovieListRecyclerViewAdapter(ImageService imageService, FragmentListener listener) {
-            this.imageService = imageService;
-            this.listener = listener;
-            setHasStableIds(true);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return movies.get(position).id;
-        }
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.movie_list_item, parent, false);
-            return new ViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(final ViewHolder holder, int position) {
-            final MovieListItem movie = movies.get(position);
-            loadImage(movie.posterPath, holder.posterImage);
-            holder.itemView.setOnClickListener((View view) -> listener.onItemClick(view, movie));
-        }
-
-        @Override
-        public int getItemCount() {
-            return movies.size();
-        }
-
-        private void loadImage(String relPath, ImageView imageView) {
-            imageService.loadImage(relPath, ImageService.SIZE_W342, imageView);
-        }
-
-        void setMovies(List<MovieListItem> movieList) {
-            Timber.d("setMovies: " + counter++);
-            DiffUtil.DiffResult diffResult = DiffUtil.calculateDiff(new DiffCallback(this.movies, movieList));
-            this.movies.clear();
-            this.movies.addAll(movieList);
-            diffResult.dispatchUpdatesTo(this);
-        }
-
-        static class ViewHolder extends RecyclerView.ViewHolder {
-            final ImageView posterImage;
-
-            ViewHolder(View itemView) {
-                super(itemView);
-                posterImage = (ImageView) itemView.findViewById(R.id.iv_poster_image);
-            }
-        }
-
-        private static class DiffCallback extends DiffUtil.Callback {
-
-            private final List<MovieListItem> newList;
-            private final List<MovieListItem> oldList;
-
-            DiffCallback(List<MovieListItem> oldList, List<MovieListItem> newList) {
-                this.newList = newList;
-                this.oldList = oldList;
-            }
-
-            @Override
-            public int getOldListSize() {
-                return oldList != null ? oldList.size() : 0;
-            }
-
-            @Override
-            public int getNewListSize() {
-                return newList != null ? newList.size() : 0;
-            }
-
-            @Override
-            public boolean areItemsTheSame(int oldPosition, int newPosition) {
-                return newList.get(newPosition) == oldList.get(oldPosition);
-            }
-
-            @Override
-            public boolean areContentsTheSame(int oldPosition, int newPosition) {
-                return newList.get(newPosition).posterPath.equals(oldList.get(oldPosition).posterPath);
-            }
-        }
-    }
 }
 
 
